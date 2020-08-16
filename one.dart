15b4452663d4f175df0139bb41170ab5a4340f29
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-17 02:08:43 +0300

    One more Commit

--- one.dart
+++ one.dart
@@ -3,55 +3,51 @@ import 'dart:convert';
 //import 'dart:html';
 import 'dart:io';
 
-double addTwoValues(double first, double second) {
-  return first + second;
-}
-
-double subTwoValues(double first, double second) {
-  return first - second;
-}
-
-double multTwoValues(double first, double second) {
-  return first * second;
-}
-
-double divTwoValues(double first, double second) {
-  return first / second;
-}
-
 void main() {
+  double addTwoValues(double first, double second) {
+    return first + second;
+  }
+
+  double subTwoValues(double first, double second) {
+    return first - second;
+  }
+
+  double multTwoValues(double first, double second) {
+    return first * second;
+  }
+
+  double divTwoValues(double first, double second) {
+    return first / second;
+  }
+
   print('Input please operation (+,-,*,/): ');
   //var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
   //String char = line.trim();
 
   //Testing operations
   //bool x = true;
-  while (true) {
-    var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
-    var str = line.trim().split(" ");
-    //print(str.runtimeType);
+  //while (true) {
+  var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+  var str = line.trim().split(" ");
+  //print(str.runtimeType);
 
-    int x = int.parse(str[0]);
-    String char = str[1];
-    int y = int.parse(str[2]);
-
-    if (char == '+') {
-      //print('1 + 1 = ${1 + 1}');
-
-    } else if (char == '-') {
-      //print('21 - 1 = ${21 - 1}');
-
-    } else if (char == '*') {
-      //print('3 * 7 = ${3 * 7}');
-
-    } else if (char == '/') {
-      //print('8 / 2 = ${8 / 2}');
-
-    } else {
-      //print('Error! Enter please some: + , - , *  or /');
-
-    }
+  //print("First - ${str[0]}, second - ${str[1]}, third - ${str[2]} !");
+  double x = double.parse(str[0]);
+  String char = str[1];
+  double y = double.parse(str[2]);
+  double total = 0;
+  if (char == "+") {
+    total = addTwoValues(x, y);
+  } else if (char == '-') {
+    total = subTwoValues(x, y);
+  } else if (char == '*') {
+    total = multTwoValues(x, y);
+  } else if (char == '/') {
+    total = divTwoValues(x, y);
+  } else {
+    print('Error! Enter please some. Example: 222 + 444');
   }
-
-  //print('This is end of programm! Goodbuy!');
+  //}
+  print("$str = $total !!! Its so easy in Dart :)");
+  print('This is end of programm! Goodbuy!');
 }
