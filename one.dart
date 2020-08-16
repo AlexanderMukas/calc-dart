4e26999f40eb861d632078aeac305622ab4c049f
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-17 00:05:24 +0300

    Second Commit of Dart file

--- one.dart
+++ one.dart
@@ -1,3 +1,13 @@
 void main() {
   print('Hello, World!');
+  // type of variables
+  //var number = 45;
+  int intValue = 100;
+  double doubleValue = 99.99;
+  num value = getValueFromServer();
+  print('int value: $intValue, double: $doubleValue, num: $value');
+}
+
+double getValueFromServer() {
+  return 3.14;
 }
