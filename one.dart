905d96b571ad3f7e8d0ea63cb1985b3605ed63f8
AlexanderMukas <alexandermukas@yahoo.com>
2020-08-17 01:21:16 +0300

    Some Commit at night

--- one.dart
+++ one.dart
@@ -1,13 +1,47 @@
-void main() {
-  print('Hello, World!');
-  // type of variables
-  //var number = 45;
-  int intValue = 100;
-  double doubleValue = 99.99;
-  num value = getValueFromServer();
-  print('int value: $intValue, double: $doubleValue, num: $value');
+//import 'dart:convert';
+import 'dart:convert';
+//import 'dart:html';
+import 'dart:io';
+
+double addTwoValues(double first, double second) {
+  return first + second;
 }
 
-double getValueFromServer() {
-  return 3.14;
+double subTwoValues(double first, double second) {
+  return first - second;
+}
+
+double multTwoValues(double first, double second) {
+  return first * second;
+}
+
+double divTwoValues(double first, double second) {
+  return first / second;
+}
+
+void main() {
+  print('Input please operation (+,-,*,/): ');
+  //var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+  //String char = line.trim();
+
+  //Testing operations
+  //bool x = true;
+  while (true) {
+    var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+    String char = line.trim();
+
+    if (char == '+') {
+      print('1 + 1 = ${1 + 1}');
+    } else if (char == '-') {
+      print('21 - 1 = ${21 - 1}');
+    } else if (char == '*') {
+      print('3 * 7 = ${3 * 7}');
+    } else if (char == '/') {
+      print('8 / 2 = ${8 / 2}');
+    } else {
+      print('Error! Enter please some: + , - , *  or /');
+    }
+  }
+
+  //print('This is end of programm! Goodbuy!');
 }
